e9563843d0b4cd890ad3d7a60574097de82a2e1e
Manuel Kalettka <manuel.kalettka@kernkonzept.com>
2022-10-18 15:26:20 +0200

    huge amount of new patches

--- PKGBUILD
+++ PKGBUILD
@@ -1,6 +1,6 @@
 pkgname=dwm-patched
 _pkgname=dwm
-pkgver=6.3.r9.gd3f93c7
+pkgver=6.4.r0.g50ad171
 pkgrel=1
 pkgdesc="A dynamic window manager for X"
 url="http://dwm.suckless.org"
@@ -14,12 +14,25 @@ provides=('dwm')
 conflicts=('dwm' 'dwm-git')
 _patches=('https://dwm.suckless.org/patches/statusallmons/dwm-statusallmons-6.2.diff'
           'dwm-focusmonmouse-6.2.diff'
-          'https://dwm.suckless.org/patches/autostart/dwm-autostart-20210120-cb3f58a.diff')
+          'https://dwm.suckless.org/patches/restartsig/dwm-restartsig-20180523-6.2.diff'
+          'https://dwm.suckless.org/patches/restoreafterrestart/dwm-restoreafterrestart-20220709-d3f93c7.diff'
+          'https://dwm.suckless.org/patches/layoutmenu/dwm-layoutmenu-6.2.diff'
+          'https://dwm.suckless.org/patches/dragmfact/dwm-dragmfact-6.2.diff'
+          'https://dwm.suckless.org/patches/cfacts/dwm-cfacts-20200913-61bb8b2.diff'
+          'https://dwm.suckless.org/patches/cfacts/dwm-cfacts_bottomstack-6.2.diff'
+          'https://dwm.suckless.org/patches/doublepressquit/dwm-doublepressquit-6.3.diff'
+)
 source=(dwm.desktop
         "$_pkgname::git+http://git.suckless.org/dwm"
         config.h
         "${_patches[@]}")
 md5sums=('939f403a71b6e85261d09fc3412269ee'
+         'SKIP'
+         'SKIP'
+         'SKIP'
+         'SKIP'
+         'SKIP'
+         'SKIP'
          'SKIP'
          'SKIP'
          'SKIP'
@@ -39,7 +52,7 @@ prepare() {
 
   for patch in "${_patches[@]}"; do
       echo "Applying patch $(basename $patch)..."
-      patch -Np1 -i "$srcdir/$(basename $patch)"
+      patch -Np1 -i "$srcdir/$(basename $patch)" --verbose
   done
 }
 
--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 #include <X11/XF86keysym.h>
+#define SESSION_FILE "/tmp/dwm-session"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -23,6 +24,9 @@ static const char *colors[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* Lockfile */
+static char lockfile[] = "/tmp/dwm.lock";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -44,6 +48,8 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
@@ -59,8 +65,9 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *layoutmenu_cmd = "dwm-layoutmenu.sh";
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *dmenunetcmd[] = {"networkmanager_dmenu", NULL};
 static const char *raisevolcmd[] = {"amixer", "-c", "1", "set", "Master", "5%+"};
@@ -74,7 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_n,      spawn,          {.v = dmenunetcmd } },
-	{ 0,          XK_F12,            spawn,          {.v = lockcmd } },
+	{ 0,                            XK_F12,    spawn,          {.v = lockcmd } },
     { 0,          XF86XK_AudioLowerVolume,     spawn,          {.v = lowervolcmd} },
     { 0,          XF86XK_AudioRaiseVolume,     spawn,          {.v = raisevolcmd} },
     { 0,          XF86XK_AudioMute,            spawn,          {.v = mutecmd} },
@@ -87,12 +94,17 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
+	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
+	{ MODKEY|ShiftMask,             XK_o,      setcfact,       {.f =  0.00} },
 	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
+	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -111,6 +123,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} },
 };
 
 /* button definitions */
@@ -118,7 +131,7 @@ static Key keys[] = {
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkLtSymbol,          0,              Button3,        layoutmenu,     {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
